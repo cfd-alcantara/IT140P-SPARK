6b1c9d3f374658b9527c1a09f03f36741b1146ca
jp-gerona <julianpetergerona@gmail.com>
2025-07-16 17:08:49 +0800

    feat: created the TimeTable composable

--- app/src/main/java/com/example/it140p_spark/ui/screens/ScheduleScreen.kt
+++ app/src/main/java/com/example/it140p_spark/ui/screens/ScheduleScreen.kt
@@ -3,16 +3,15 @@ package com.example.it140p_spark.ui.screens
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.example.it140p_spark.ui.components.TimeTable
 
+/* todo: display the finalized list of courses in this Screen
+*        else, will display a message that "you are not enrolled" */
 @Composable
 fun ScheduleScreen(padding: PaddingValues) {
     Box(modifier = Modifier.padding(padding)) {
-        // todo: Schedule content
-        Text(
-            text = "Schedule Screen"
-        )
+        TimeTable()
     }
 }
